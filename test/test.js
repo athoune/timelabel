ae97d9986395b8473518030fcb7915b8a5ce3842
Mathieu Lecarme <mlecarme@bearstech.com>
2013-05-28 00:08:41 +0200

    Audacious time labeling.

--- test/test.js
+++ test/test.js
@@ -13,10 +13,18 @@ test("periods", function() {
 });
 
 test("labels", function() {
-    var delta = new timelabel(new Date(1984, 11, 13), new Date(2013, 5, 11));
-    var l = delta.labels(20);
-    l.forEach(function(d) {
-        console.log(d.format());
-    })
-    equal(l.length, 29);
+    var delta = new timelabel(new Date(1984, 11, 13), new Date(2013, 5, 11)),
+        l = delta.labels();
+    equal(l.length, 3);
+
+    delta = new timelabel(new Date(2013, 0, 2), new Date(2013, 3, 15));
+    l = delta.labels();
+    equal(l.length, 4);
+
+    delta = new timelabel(new Date(1976, 0, 2), new Date(2013, 3, 15));
+    l = delta.labels();
+    equal(l.length, 4);
+    delta = new timelabel(new Date(1903, 0, 2), new Date(2013, 3, 15));
+    l = delta.labels();
+    equal(l.length, 2);
 });
